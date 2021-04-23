718d6fe2ebb402fb564a536c651f3e6fecffa53c
Trynda-LOL <bm33518@gmail.com>
2021-04-23 11:33:40 -0300

    component Player and update component Header

--- podcastr-next/src/components/Header/index.tsx
+++ podcastr-next/src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR';
 
 export function Header() {
-    const currentDate = format(new Date(),  'EEEEEE, d MMMM', {
+    const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
         locale: ptBR
     });
 
